2609e7aa8f7c5bc972d84a1a2de421d53f54321a
Valerii Glavatskih <81376307+m0rfox@users.noreply.github.com>
2021-03-31 20:39:50 +0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
 	System.out.println("Hello World");
-	System.out.println("Hello <EPAM>");
+	System.out.println("Hello <EPAM>"); //Создаем конфликт для операции pull
 	System.out.println("Hello pull");  // Строка изменена через браузер 
     }
 }
