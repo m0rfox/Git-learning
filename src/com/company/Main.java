c5fbe468b3455ef059caea5dc9462f39c6804209
Valerii Glavatskih <glavatskihvalerii@yandex.ru>
2021-03-31 21:17:32 +0400

    Commit #8 (newFeature2)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,6 @@ public class Main {
 	System.out.println("Hello World");
 	System.out.println("Hello <EPAM>");
 	int newFeature1 = 10; // commit one for rebase
+	int newFeature2 = 20; // commit two for rebase
     }
 }
