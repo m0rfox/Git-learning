032ce1cd85e2b11f397843ca16740f5b386086ee
Valerii Glavatskih <glavatskihvalerii@yandex.ru>
2021-03-31 21:33:44 +0400

    Commit #10 (Commit for revert)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,5 +8,6 @@ public class Main {
 	System.out.println("Hello <EPAM>");
 	int newFeature1 = 10; // commit one for rebase
 	int newFeature2 = 20; // commit two for rebase
+	boolean x = false; // commit for revert
     }
 }
